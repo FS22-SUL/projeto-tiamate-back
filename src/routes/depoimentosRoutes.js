ebf3ea270bfce8ec420b4d45163ffe9388b8abcf
Rafael <fconrafael@gmail.com>
2025-07-14 16:40:22 -0300

    update depoimentos

---
+++ src/routes/depoimentosRoutes.js
@@ -0,0 +1,26 @@
+const { buscarTodos, buscarUm, criar, editar, deletar } = require("../controllers/depoimentosController");
+const { rotaProtegida } = require("../utils");
+
+const router = require("express").Router();
+
+router.get("/", async (req, res) => {
+    res.send(await buscarTodos());
+});
+
+router.post("/", rotaProtegida, async(req, res) => {
+    res.send(await criar(req));
+});
+
+router.post("/", rotaProtegida, async(req, res) => {
+    res.send(await criar(req));
+});
+
+router.put("/:id", rotaProtegida, async(req, res) => {
+    res.send(await editar(req.params.id, req));
+});
+
+router.delete("/:id", rotaProtegida, async(req, res) => {
+    res.send(await deletar(req.params.id));
+});
+
+module.exports = router;
