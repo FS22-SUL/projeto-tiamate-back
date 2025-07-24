24000280732e2f6ee8bb1a6193d931214c8a6005
wells99 <9.9wellington9.9@gmail.com>
2025-07-23 22:45:33 -0300

    falha de consistencia de imagens resolvida em produtos

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+  cloud_name: 'dcnkgp6zv',
+  api_key: '722999845121343',
+  api_secret: 'Zgeff8kKeFoHoeTsVJ8DhRsOOQ0'
+});
+
+module.exports = cloudinary;
